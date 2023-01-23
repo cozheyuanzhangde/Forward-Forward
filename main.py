473582949ef68bd07c25cd630bcfa8dbdb5655ea
Zheyuan(Brian) Zhang <cozzyde@gmail.com>
2023-01-23 08:44:32 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,36 +1,25 @@
-import matplotlib.pyplot as plt
 import torch
 from utils import *
 from model import *
-
-    
-def visualize_sample(data, name='', idx=0):
-    reshaped = data[idx].cpu().reshape(28, 28)
-    plt.figure(figsize = (4, 4))
-    plt.title(name)
-    plt.imshow(reshaped, cmap="gray")
-    plt.show()
-    
     
 if __name__ == "__main__":
     torch.manual_seed(1234)
     train_loader, test_loader = MNIST_loaders()
 
-    net = Net([784, 500, 500])
-    x, y = next(iter(train_loader))
-    x, y = x.cuda(), y.cuda()
-    x_pos = overlay_y_on_x(x, y)
-    rnd = torch.randperm(x.size(0))
-    x_neg = overlay_y_on_x(x, y[rnd])
+    net = FFNet([784, 2000, 1000, 500, 200, 500, 1000])
+    train_images, train_labels = next(iter(train_loader))
+    train_images, train_labels = train_images.cuda(), train_labels.cuda()
+    data_pos = create_data_pos(train_images, train_labels)
+    data_neg = create_data_neg(train_images, train_labels)
     
-    for data, name in zip([x, x_pos, x_neg], ['orig', 'pos', 'neg']):
-        visualize_sample(data, name)
+    # for data, name in zip([train_images, data_pos, data_neg], ['orig', 'pos', 'neg']):
+    #     visualize_sample(data, name)
     
-    net.train(x_pos, x_neg)
+    net.train_1(train_loader)
 
-    print('train error:', 1.0 - net.predict(x).eq(y).float().mean().item())
+    # print('train error:', 1.0 - net.predict(train_images).eq(train_labels).float().mean().item())
 
-    x_te, y_te = next(iter(test_loader))
-    x_te, y_te = x_te.cuda(), y_te.cuda()
+    # test_images, test_labels = next(iter(test_loader))
+    # test_images, test_labels = test_images.cuda(), test_labels.cuda()
 
-    print('test error:', 1.0 - net.predict(x_te).eq(y_te).float().mean().item())
+    # print('test error:', 1.0 - net.predict(test_images).eq(test_labels).float().mean().item())
